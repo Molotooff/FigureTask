34c5b6534e5016714c422c297fc28df759da32b3
Molotooff <kolljei@gmail.com>
2024-08-07 21:13:32 +0300

    feat: Добавлено решение задачи по пересечению полигонов

--- FigureTask/Program.cs
+++ FigureTask/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FigureTask
 {
@@ -14,25 +11,53 @@ namespace FigureTask
             double areaOfTheFirstFigure = 0;
             double areaOfTheSecondFigure = 0;
 
-            double[,] triangleCoordinates11 = { { 12, 15 }, { 0, 0 }, { -1, -13 } };
-            double[,] triangleCoordinates12 = { { 123, 153 }, { 30, 30 }, { -31, -133 } };
-            double[,] retangleCoordinates11 = { { 12, 15 }, { 0, 0 }, { -1, -13 }, { -21, -213 } };
+
+            double[,] triangleCoordinates11 = { { 0, 0 }, { 4, 0 }, { 2, 3 } };
+            double[,] triangleCoordinates12 = { { 2, 1 }, { 6, 1 }, { 4, 4 } };
+            double[,] rectangleCoordinates11 = { { 1, 1 }, { 3, 1 }, { 3, 3 }, { 1, 3 } };
 
             Figure triangle11 = new Triangle(triangleCoordinates11);
             Figure triangle12 = new Triangle(triangleCoordinates12);
-            Figure retangle11 = new Rectangle(retangleCoordinates11);
+            Figure rectangle11 = new Rectangle(rectangleCoordinates11);
 
-            var Figure1 = new Check(new List<Figure>() { triangle11, triangle12, retangle11 });
+            var Figure1 = new Check(new List<Figure>() { triangle11, triangle12, rectangle11 });
 
-            double[,] retangleCoordinates21 = { { 123, 153 }, { 302, 30 }, { -31, -133 }, { -1, -13 } };
-            double[,] retangleCoordinates22 = { { 13, 315 }, { 320, 330 }, { -133, -13 }, { -1, -123 } };
-            double[,] triangleCoordinates21 = { { 182, 15 }, { 50, 80 }, { -41, -183 } };
 
-            Figure retangle21 = new Rectangle(retangleCoordinates21);
-            Figure retangle22 = new Rectangle(retangleCoordinates22);
+            double[,] rectangleCoordinates21 = { { 2, 2 }, { 5, 2 }, { 5, 4 }, { 2, 4 } };
+            double[,] rectangleCoordinates22 = { { 3, 3 }, { 6, 3 }, { 6, 5 }, { 3, 5 } };
+            double[,] triangleCoordinates21 = { { 4, 0 }, { 8, 0 }, { 6, 3 } };
+
+            Figure rectangle21 = new Rectangle(rectangleCoordinates21);
+            Figure rectangle22 = new Rectangle(rectangleCoordinates22);
             Figure triangle21 = new Triangle(triangleCoordinates21);
 
-            var Figure2 = new Check(new List<Figure>() { retangle21, retangle22, triangle21 });
+            var Figure2 = new Check(new List<Figure>() { rectangle21, rectangle22, triangle21 });
+
+            double[,] triangleCoordinates31 = { { 1, 1 }, { 5, 1 }, { 3, 4 } };
+            Figure triangle31 = new Triangle(triangleCoordinates31);
+
+            var Figure3 = new Check(new List<Figure>() { triangle31 });
+
+            var intersectionArea1and2 = GetIntersectionArea(Figure1.Figures, Figure2.Figures);
+            var intersectionArea1and3 = GetIntersectionArea(Figure1.Figures, Figure3.Figures);
+            var intersectionArea2and3 = GetIntersectionArea(Figure2.Figures, Figure3.Figures);
+
+            var unionArea1and2 = GetUnionArea(Figure1.Figures, Figure2.Figures);
+            var unionArea1and3 = GetUnionArea(Figure1.Figures, Figure3.Figures);
+            var unionArea2and3 = GetUnionArea(Figure2.Figures, Figure3.Figures);
+
+            Console.WriteLine($"Область пересечения Figure1 и Figure2: {intersectionArea1and2}");
+            Console.WriteLine($"Область пересечения Figure1 и Figure3: {intersectionArea1and3}");
+            Console.WriteLine($"Область пересечения Figure2 и Figure3: {intersectionArea2and3}");
+
+
+            Console.WriteLine($"Область объединения с пересечением Figure1 и Figure2: {unionArea1and2}");
+            Console.WriteLine($"Область объединения с пересечением Figure1 и Figure3: {unionArea1and3}");
+            Console.WriteLine($"Область объединения с пересечением Figure2 и Figure3: {unionArea2and3}");
+
+            Console.WriteLine($"Область объединения без пересечения Figure1 и Figure2: {unionArea1and2 - intersectionArea1and2}");
+            Console.WriteLine($"Область объединения без пересечения Figure1 и Figure3: {unionArea1and3 - intersectionArea1and3}");
+            Console.WriteLine($"Область объединения без пересечения Figure2 и Figure3: {unionArea2and3 - intersectionArea2and3}");
 
             if (Figure1.IsValid() == 0 && Figure2.IsValid() != 0)
             {
@@ -55,8 +80,10 @@ namespace FigureTask
 
                 foreach (var figure in Figure2.Figures)
                 {
-                    areaOfTheFirstFigure += figure.GetArea();
+                    areaOfTheSecondFigure += figure.GetArea();
                 }
+
+
 
                 if (areaOfTheFirstFigure > areaOfTheSecondFigure)
                 {
@@ -72,12 +99,171 @@ namespace FigureTask
                 }
             }
         }
+
+        public static double GetIntersectionArea(List<Figure> figures1, List<Figure> figures2)
+        {
+            var intersectionPoints = new List<Point>();
+
+            foreach (var figure1 in figures1)
+            {
+                foreach (var figure2 in figures2)
+                {
+                    intersectionPoints.AddRange(GetIntersection(figure1.GetPoints(), figure2.GetPoints()));
+                }
+            }
+
+            return CalculateArea(intersectionPoints);
+        }
+
+        public static double GetUnionArea(List<Figure> figures1, List<Figure> figures2)
+        {
+            var unionPoints = new List<Point>();
+
+            foreach (var figure in figures1)
+            {
+                unionPoints.AddRange(figure.GetPoints());
+            }
+
+            foreach (var figure in figures2)
+            {
+                unionPoints.AddRange(figure.GetPoints());
+            }
+
+            return CalculateArea(unionPoints.Distinct().ToList());
+        }
+
+        public static List<Point> GetIntersection(List<Point> polygon1, List<Point> polygon2)
+        {
+            var intersection = new List<Point>();
+
+            for (int i = 0; i < polygon1.Count; i++)
+            {
+                int nextI = (i + 1) % polygon1.Count;
+                for (int j = 0; j < polygon2.Count; j++)
+                {
+                    int nextJ = (j + 1) % polygon2.Count;
+                    var intersectPoint = GetIntersectionPoint(
+                        polygon1[i], polygon1[nextI],
+                        polygon2[j], polygon2[nextJ]);
+                    if (intersectPoint != null)
+                    {
+                        intersection.Add(intersectPoint.Value);
+                    }
+                }
+            }
+
+            foreach (var point in polygon1)
+            {
+                if (IsPointInPolygon(point, polygon2))
+                {
+                    intersection.Add(point);
+                }
+            }
+
+            foreach (var point in polygon2)
+            {
+                if (IsPointInPolygon(point, polygon1))
+                {
+                    intersection.Add(point);
+                }
+            }
+
+            return intersection.Distinct().ToList();
+        }
+
+        private static Point? GetIntersectionPoint(Point p1, Point p2, Point q1, Point q2)
+        {
+            double a1 = p2.Y - p1.Y;
+            double b1 = p1.X - p2.X;
+            double c1 = a1 * p1.X + b1 * p1.Y;
+
+            double a2 = q2.Y - q1.Y;
+            double b2 = q1.X - q2.X;
+            double c2 = a2 * q1.X + b2 * q1.Y;
+
+            double delta = a1 * b2 - a2 * b1;
+            if (Math.Abs(delta) < 1e-10)
+            {
+                return null;
+            }
+
+            double x = (b2 * c1 - b1 * c2) / delta;
+            double y = (a1 * c2 - a2 * c1) / delta;
+
+            if (IsPointOnLineSegment(new Point(x, y), p1, p2) && IsPointOnLineSegment(new Point(x, y), q1, q2))
+            {
+                return new Point(x, y);
+            }
+
+            return null;
+        }
+
+        private static bool IsPointOnLineSegment(Point p, Point p1, Point p2)
+        {
+            return p.X >= Math.Min(p1.X, p2.X) && p.X <= Math.Max(p1.X, p2.X)
+                   && p.Y >= Math.Min(p1.Y, p2.Y) && p.Y <= Math.Max(p1.Y, p2.Y);
+        }
+
+        private static bool IsPointInPolygon(Point point, List<Point> polygon)
+        {
+            int count = 0;
+            for (int i = 0; i < polygon.Count; i++)
+            {
+                int next = (i + 1) % polygon.Count;
+                Point p1 = polygon[i];
+                Point p2 = polygon[next];
+
+                if (point.Y > Math.Min(p1.Y, p2.Y) && point.Y <= Math.Max(p1.Y, p2.Y)
+                    && point.X <= Math.Max(p1.X, p2.X) && p1.Y != p2.Y)
+                {
+                    double xinters = (point.Y - p1.Y) * (p2.X - p1.X) / (p2.Y - p1.Y) + p1.X;
+                    if (p1.X == p2.X || point.X <= xinters)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count % 2 != 0;
+        }
+
+        private static double CalculateArea(List<Point> points)
+        {
+            if (points.Count < 3) return 0;
+
+            double area = 0;
+            int j = points.Count - 1;
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                area += (points[j].X + points[i].X) * (points[j].Y - points[i].Y);
+                j = i;
+            }
+
+            return Math.Abs(area / 2.0);
+        }
+
+        public struct Point
+        {
+            public double X { get; }
+            public double Y { get; }
+
+            public Point(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public override string ToString() => $"({X}, {Y})";
+        }
     }
+
     abstract class Figure
     {
         public abstract double GetArea();
-        public abstract double[,] GetCoordinates();
+        public abstract List<Program.Point> GetPoints();
     }
+
     class Triangle : Figure
     {
         public double[,] coordinates;
@@ -89,6 +275,7 @@ namespace FigureTask
                 throw new ArgumentException("Массив должен быть размером [3,2]");
             coordinates = inputCoordinates;
         }
+
         public override double GetArea()
         {
             double x1 = coordinates[0, 0], y1 = coordinates[0, 1];
@@ -99,11 +286,17 @@ namespace FigureTask
             return area;
         }
 
-        public override double[,] GetCoordinates()
+        public override List<Program.Point> GetPoints()
         {
-            return coordinates;
+            var points = new List<Program.Point>();
+            for (int i = 0; i < coordinates.GetLength(0); i++)
+            {
+                points.Add(new Program.Point(coordinates[i, 0], coordinates[i, 1]));
+            }
+            return points;
         }
     }
+
     class Rectangle : Figure
     {
         public double[,] coordinates;
@@ -115,6 +308,7 @@ namespace FigureTask
                 throw new ArgumentException("Массив должен быть размером [4,2]");
             coordinates = inputCoordinates;
         }
+
         public override double GetArea()
         {
             double length = Math.Abs(coordinates[1, 0] - coordinates[0, 0]);
@@ -122,16 +316,23 @@ namespace FigureTask
             area = length * width;
             return area;
         }
-        public override double[,] GetCoordinates()
-        {
-            return coordinates;
-        }
 
+        public override List<Program.Point> GetPoints()
+        {
+            var points = new List<Program.Point>();
+            for (int i = 0; i < coordinates.GetLength(0); i++)
+            {
+                points.Add(new Program.Point(coordinates[i, 0], coordinates[i, 1]));
+            }
+            return points;
+        }
     }
+
     abstract class PropertiesOfFigures
     {
         public abstract int IsValid();
     }
+
     class Check : PropertiesOfFigures
     {
         public List<Figure> Figures { get; }
@@ -159,14 +360,14 @@ namespace FigureTask
 
         private bool AreFiguresConnected(Figure figure1, Figure figure2)
         {
-            double[,] coords1 = figure1.GetCoordinates();
-            double[,] coords2 = figure2.GetCoordinates();
+            var coords1 = figure1.GetPoints();
+            var coords2 = figure2.GetPoints();
 
-            for (int i = 0; i < coords1.GetLength(0); i++)
+            for (int i = 0; i < coords1.Count; i++)
             {
-                for (int j = 0; j < coords2.GetLength(0); j++)
+                for (int j = 0; j < coords2.Count; j++)
                 {
-                    if (coords1[i, 0] == coords2[j, 0] && coords1[i, 1] == coords2[j, 1])
+                    if (coords1[i].X == coords2[j].X && coords1[i].Y == coords2[j].Y)
                     {
                         return true;
                     }
@@ -176,3 +377,6 @@ namespace FigureTask
         }
     }
 }
+
+
+
